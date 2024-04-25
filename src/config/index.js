99ea188b0fe3271a4b99fecc00f67a9732c17335
KhondamirMakhmudov <xondamir.maxmudov.01@mail.ru>
2024-04-25 11:45:25 +0500

    changed api_url

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 export const config = {
-    API_URL: 'https://backend-market.tmsiti.uz/',
-    FILE_URL: 'https://backend-market.tmsiti.uz/static/categories/materials/',
-    DEFAULT_APP_LANG: 'uz'
+  API_URL: "https://jetmind.uz/",
+  FILE_URL: "https://backend-market.tmsiti.uz/static/categories/materials/",
+  DEFAULT_APP_LANG: "uz",
 };
